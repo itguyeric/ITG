badd26ac7078c9aca91b66eb8893c486f2d0c1a6
Eric The IT Guy <erichendricks@fastmail.com>
2024-05-03 18:38:25 -0500

    Added kptach script

---
+++ demo-magic/kpatch.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+. ../demo-magic.sh
+
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+# TYPE_SPEED=20
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+
+# text color
+# DEMO_CMD_COLOR=$BLACK
+
+# hide the evidence
+clear
+
+
+# put your demo awesomeness here
+
+pei "cat /etc/redhat-release"
+
+pe "dnf info kptch"
+
+pe "kpatch list"
+
+pe "uname -r"
+
+pe "dnf list available kpatch-patch*284*"
+
+pe "dnf -y install "kpatch-patch = $(uname -r)""
+
+pe "kpatch list"
+
+pe "rpm -q --changelog $(rpm -qa | grep kpatch-patch)"
+
+# show a prompt so as not to reveal our true nature after
+# the demo has concluded
+p ""
