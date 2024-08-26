08934b2e5a53897c7c33a6f74e8357c7bf1a877a
Eric the IT Guy <eric@itguyeric.com>
2024-08-26 10:10:25 -0500

    rebuilt ansible playbooks

--- scripts/itg-tmux.sh
+++ scripts/itg-tmux.sh
@@ -5,25 +5,28 @@ SESSION="jarvis"
 # Start a new tmux session
 tmux new-session -d -s $SESSION
 
-# Create windows and run commands
+# 0:local
 tmux rename-window -t $SESSION:0 'local'
 tmux send-keys -t $SESSION:0 'bash; clear' C-m
 
+# 1:unraid
 tmux new-window -t $SESSION:1 -n 'unraid'
 tmux send-keys -t $SESSION:1 'ssh -i ~/.ssh/id_rsa_unraid root@itg01; clear' C-m
 
+# 2:rhel
 tmux new-window -t $SESSION:2 -n 'rhel'
 tmux send-keys -t $SESSION:2 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel09a; clear' C-m
-tmux split-window -t $SESSION:2 -v
+tmux split-window -t $SESSION:2 -h
 tmux send-keys -t $SESSION:2.1 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel09b; clear' C-m
-tmux split-window -t $SESSION:2.0 -h
+tmux split-window -t $SESSION:2.0 -v
 tmux send-keys -t $SESSION:2.2 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel08a; clear' C-m
 tmux split-window -t $SESSION:2.2 -v
 tmux send-keys -t $SESSION:2.3 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel08b; clear' C-m
 
-# Set the layout to tiled (2x2 grid)
-tmux select-layout -t $SESSION:2 tiled
+# Manually enforce the layout to even-horizontal for 2x2 grid
+tmux select-layout -t $SESSION:2 even-horizontal
 
+# 3:k8s
 tmux new-window -t $SESSION:3 -n 'k8s'
 tmux send-keys -t $SESSION:3 'cd ~/git/itg/kube; bash; clear' C-m
 
