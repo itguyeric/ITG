e906e5e231a11e8d60ad8e521a3f042a02d85a03
Eric the IT Guy <eric@itguyeric.com>
2024-08-26 01:48:54 -0500

    updated tmux config

--- scripts/itg-tmux.sh
+++ scripts/itg-tmux.sh
@@ -11,16 +11,19 @@ tmux send-keys -t $SESSION:0 'bash; clear' C-m
 
 tmux new-window -t $SESSION:1 -n 'unraid'
 tmux send-keys -t $SESSION:1 'ssh -i ~/.ssh/id_rsa_unraid root@itg01' C-m
-tmux send-keys -t $SESSION:1 'clear' C-m
+tmux send-keys -t $SESSION:1 'export TERM=xterm-256color; clear' C-m
 
 tmux new-window -t $SESSION:2 -n 'rhel'
 tmux send-keys -t $SESSION:2 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel09a' C-m
 tmux split-window -t $SESSION:2 -v
 tmux send-keys -t $SESSION:2.1 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel09b' C-m
-tmux split-window -t $SESSION:2.1 -h
+tmux split-window -t $SESSION:2.0 -h
 tmux send-keys -t $SESSION:2.2 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel08a' C-m
 tmux split-window -t $SESSION:2.2 -v
 tmux send-keys -t $SESSION:2.3 'ssh -i ~/.ssh/id_rsa_ans ansible@rhel08b' C-m
+
+# Ensure the layout is set to tiled (2x2 grid) for proper quadrant layout
+tmux select-layout -t $SESSION:2 tiled
 
 # Add the clear command for each pane after SSH
 tmux send-keys -t $SESSION:2 'clear' C-m
