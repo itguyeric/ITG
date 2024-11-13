8c16c9d1700b39d95387e669a4c086b71e0b3995
Eric The IT Guy <erichendricks@fastmail.com>
2024-11-13 10:08:26 -0600

    corrected command

--- scripts/rclone-friday.sh
+++ scripts/rclone-friday.sh
@@ -17,6 +17,6 @@ for dir in "${SYNC_DIRS[@]}"; do
   
   for target in "${TARGETS[@]}"; do
     #/opt/homebrew/bin/rclone bisync "$SOURCE_PATH" "$target/${dir}" --log-file="$LOG_FILE" --log-level INFO --resync --copy-links --checksum
-    /opt/homebrew/bin/rclone sync "$SOURCE_PATH" "$target/${dir}" --log-file="$LOG_FILE" --log-level INFO --resync --copy-links --checksum
+    /opt/homebrew/bin/rclone sync "$SOURCE_PATH" "$target/${dir}" --log-file="$LOG_FILE" --log-level INFO --copy-links --checksum
   done
 done
