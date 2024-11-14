057182fb475a7d9d4f2a611d546421ba4dbc0e59
Eric The IT Guy <erichendricks@fastmail.com>
2024-11-14 16:27:57 -0600

    removed logging

--- scripts/rclone-friday.sh
+++ scripts/rclone-friday.sh
@@ -8,8 +8,8 @@ TARGETS=("nextcloud:" "gDrive-EH:")
 # List of directories to sync
 SYNC_DIRS=("Desktop" "Documents" "Downloads" "Movies" "Music" "Pictures" "Public" "Support" "Templates" "Videos")
 
-# Log file location
-LOG_FILE="/Users/ehendricks/tmp/rclone.log"
+## Log file location
+#LOG_FILE="/Users/ehendricks/tmp/rclone.log"
 
 # Sync loop
 for dir in "${SYNC_DIRS[@]}"; do
@@ -17,6 +17,6 @@ for dir in "${SYNC_DIRS[@]}"; do
   
   for target in "${TARGETS[@]}"; do
     #/opt/homebrew/bin/rclone bisync "$SOURCE_PATH" "$target/${dir}" --log-file="$LOG_FILE" --log-level INFO --resync --copy-links --checksum
-    /opt/homebrew/bin/rclone sync "$SOURCE_PATH" "$target/${dir}" --log-file="$LOG_FILE" --log-level INFO --copy-links --checksum
+    /opt/homebrew/bin/rclone sync "$SOURCE_PATH" "$target/${dir}" --copy-links --checksum
   done
 done
