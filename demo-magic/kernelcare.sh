4cb5191230eb870d7f124d830fa120cd69d41312
Ansible <ansible@itg-tst-alma9.home.itguyeric.com>
2025-07-21 16:33:39 -0500

    created kernelcare demo

---
+++ demo-magic/kernelcare.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+. ~/git/demo-magic/demo-magic.sh
+
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+# TYPE_SPEED=20
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+
+# text color
+# DEMO_CMD_COLOR=$BLACK
+
+# setup
+
+echo "Configuring demo..."
+
+# hide the evidence
+clear
+
+# put your demo awesomeness here
+
+pei "cat /etc/almalinux-release"
+
+pei curl -s -L https://kernelcare.com/installer | sudo bash
+
+pe "clear"
+
+pei sudo kcarectl --info
+
+pei sudo kcarectl --register 123456-1234-5678 2> /dev/null
+# hidden server change
+sudo kcarectl --register 47044768-0917-4f63-9585-62f7959c1328
+
+pe "clear"
+
+pei "uname -r"
+
+pei "sudo kcarectl --update"
+
+pe "clear"
+
+pei "kcarectl --patch-info"
+
+# show a prompt so as not to reveal our true nature after
+# the demo has concluded
+p ""
